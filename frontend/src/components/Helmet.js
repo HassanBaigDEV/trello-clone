f726f4715055c9470cb0fc921c58f84899bbc51b
Hassan Baig <94882385+HassanBaigDEV@users.noreply.github.com>
2024-10-06 04:28:45 +0500

    Delete frontend directory

--- frontend/src/components/Helmet.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { Helmet as ReactHelmet } from 'react-helmet';
-
-const Helmet = ({ title }) => (
-  <ReactHelmet title={`${title} | Project-Manager`} />
-);
-
-export default Helmet;
