54fac39152175daebc58723c2a81b6a417437b46
HassanBaigDEV <hassanbaig923@gmail.com>
2024-10-06 15:16:59 +0500

    bug fix

--- server.js
+++ server.js
@@ -33,12 +33,12 @@ app.use("/api/images", images);
 
 const __dirname = path.resolve();
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "/frontend/build")));
-  app.get("*", (req, res) =>
-    res.sendFile(path.resolve(__dirname, "frontend/build/index.html"))
-  );
-}
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static(path.join(__dirname, "/frontend/build")));
+//   app.get("*", (req, res) =>
+//     res.sendFile(path.resolve(__dirname, "frontend/build/index.html"))
+//   );
+// }
 const PORT = process.env.PORT || 5000;
 
 server.listen(
